99b588bfb3e8d11cf6e03fa1d2e3a49da701a0da
devconcept <devconcept@outlook.com>
2019-09-14 15:49:39 -0400

    Completed route validation

--- validations/routes/gateways.js
+++ validations/routes/gateways.js
@@ -1,16 +1,36 @@
 const { body, param, query } = require('express-validator');
 
+const pages = [10, 25, 50];
+
 module.exports = {
   getAllGatewaysValidation: [
-    query('page').isInt({ min: 1, allow_leading_zeroes: false }),
-    query('size').isIn([10, 25, 50]),
+    query('page')
+      .optional({ nullable: false, checkFalsy: false })
+      .isInt({ min: 1, allow_leading_zeroes: false }).withMessage('The field page must be integer')
+      .if(query('next').exists()).not().exists().withMessage('Only one of page or next must be provided'),
+    query('size')
+      .optional({ nullable: false, checkFalsy: false })
+      .isIn(pages).withMessage(`The field must be one of ${pages.join(',')}`)
+      .toInt(10),
+    query('next')
+      .optional({ nullable: false, checkFalsy: false })
+      .isMongoId().withMessage('The field next must be a valid MongoId value')
+      .if(query('query').exists()).not().exists().withMessage('Only one of page or next must be provided'),
   ],
   getGatewayValidation: [
-    param('id').exists({ checkFalsy: true }),
+    param('gatewayId')
+      .exists({ checkFalsy: true, checkNull: true })
+      .isMongoId().withMessage('The parameter gatewayId must be a valid MongoId value'),
   ],
   addGatewayValidation: [
-    body('name').exists({ checkFalsy: true }),
-    body('serial').exists({ checkFalsy: true }),
-    body('ipv4').isIP(4),
+    body('name')
+      .exists({ checkFalsy: true, checkNull: true }).withMessage('The field name is required')
+      .isString().withMessage('The the field name must be a string'),
+    body('serial')
+      .exists({ checkFalsy: true, checkNull: true }).withMessage('The field serial is required')
+      .isString().withMessage('The the field serial must be a string'),
+    body('ipv4')
+      .exists({ checkFalsy: true, checkNull: true }).withMessage('The field ipv4 is required')
+      .isIP(4).withMessage('The field ipv4 must be a valid ipv4 address'),
   ],
 };
