f47a6c0c59445f29941af316e622be2787d88c07
devconcept <devconcept@outlook.com>
2019-09-14 11:06:02 -0400

    Added info and error loggers through winston and debug

---
+++ helpers/loggers.js
@@ -0,0 +1,41 @@
+const winston = require('winston');
+const Transport = require('winston-transport');
+const debugLog = require('debug')('gateways:log');
+const debugError = require('debug')('gateways:error');
+
+class DebugTransport extends Transport {
+  constructor() {
+    super();
+  }
+
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info);
+    });
+
+    if (info.level === 'error') {
+      debugError(info.message);
+    } else {
+      debugLog(info.message);
+    }
+    callback();
+  }
+}
+
+const logger = winston.createLogger({
+  transports: [
+      // TODO: Add production transport
+      new DebugTransport()
+  ]
+});
+
+/* eslint-disable-next-line no-console */
+debugLog.log = console.log.bind(console);
+
+module.exports.logMessage = function logMessage(data) {
+  logger.log('info', data);
+};
+
+module.exports.logError = function logError(data) {
+  logger.log('error', data);
+};
