c6dabbda40c463d60d27bdaefd61f7dd62189718
devconcept <devconcept@outlook.com>
2019-09-14 14:10:08 -0400

    Added tight schema validation

--- validations/database/gateways.js
+++ validations/database/gateways.js
@@ -13,11 +13,13 @@ module.exports = {
       },
       ipv4: {
         bsonType: 'string',
+        pattern: '^(?:([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$',
         description: 'must be a string and is required',
       },
       peripherals: {
         bsonType: 'array',
         description: 'must be a array of objects containing uid, vendor, created and status',
+        maxItems: 10,
         items: {
           bsonType: 'object',
           required: ['uid', 'vendor', 'created', 'status'],
