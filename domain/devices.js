13605b949a566c73e2b956e3986ed522b3593bf8
devconcept <devconcept@outlook.com>
2019-09-15 07:24:23 -0400

    Add device query on only one command

--- domain/devices.js
+++ domain/devices.js
@@ -3,24 +3,13 @@ const { mergeMap } = require('rxjs/operators');
 const { ObjectID } = require('mongodb');
 
 module.exports.addDevice = function addDevice(db, gatewayId, device) {
-  const collection = db.collection('gateways');
-  return from(collection.findOne({ _id: ObjectID(gatewayId) })).pipe(
-    mergeMap((gateway) => {
-      if (!gateway) {
-        return throwError('Not found');
-      }
-      if (gateway.devices.find((d) => d.uid === device.uid)) {
-        return throwError('Already exists');
-      }
-      return collection.updateOne({ _id: ObjectID(gatewayId), 'devices.uid': { $ne: device.uid } }, { $push: { devices: device } });
-    }),
+  return from(db.collection('gateways').updateOne({ _id: ObjectID(gatewayId), 'devices.uid': { $ne: device.uid } }, { $push: { devices: device } })).pipe(
     mergeMap((result) => {
       if (result.modifiedCount) {
         return of(device);
       }
       return throwError('Already exists');
-    }),
-  );
+    }));
 };
 
 module.exports.removeDeviceInGateway = function removeDeviceInGateway(db, gatewayId, deviceId) {
@@ -28,13 +17,11 @@ module.exports.removeDeviceInGateway = function removeDeviceInGateway(db, gatewa
     $pull: { devices: { uid: deviceId } },
   })).pipe(
     mergeMap((result) => {
-      if (!result.matchedCount) {
-        return throwError('Not found');
+      if (result.modifiedCount) {
+        return of(deviceId);
+
       }
-      if (!result.modifiedCount) {
-        return throwError('Not found');
-      }
-      return of(deviceId);
+      return throwError('Not found');
     }),
   );
 };
