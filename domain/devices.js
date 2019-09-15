fc118af9707fe916d769ae310cb94d94166876d4
devconcept <devconcept@outlook.com>
2019-09-15 06:18:58 -0400

    Fixed add device query unique enforcement

--- domain/devices.js
+++ domain/devices.js
@@ -1,17 +1,25 @@
-const { from } = require('rxjs');
-const { map } = require('rxjs/operators');
+const { from, throwError } = require('rxjs');
+const { mergeMap, of } = require('rxjs/operators');
 const { ObjectID } = require('mongodb');
 
 module.exports.addDevice = function addDevice(db, gatewayId, device) {
-  return from(db.collection('gateways').findOneAndUpdate({ _id: ObjectID(gatewayId) }, {
-    $push: {
-      devices: {
-        $each: [device],
-        // $slice: -10,
-      },
-    },
-  }, { returnOriginal: false, projection: { devices: 1 } })).pipe(
-    map((operation) => operation.value.devices.find((d) => d.uid === device.uid)),
+  const collection = db.collection('gateways');
+  return from(collection.findOne({ _id: ObjectID(gatewayId) })).pipe(
+    mergeMap((gateway) => {
+      if (!gateway) {
+        return throwError('Not found');
+      }
+      if (gateway.devices.find((d) => d.uid === device.uid)) {
+        return throwError('Already exists');
+      }
+      return collection.updateOne({ _id: ObjectID(gatewayId), 'devices.uid': { $ne: device.uid } }, { $push: { devices: device } });
+    }),
+    mergeMap((result) => {
+      if (result.modifiedCount) {
+        return of(device);
+      }
+      return throwError('Already exists');
+    }),
   );
 };
 
